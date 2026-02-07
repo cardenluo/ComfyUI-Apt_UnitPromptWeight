8bb10d9eca3066c4c93696781165389aba658f04
cardenluo <133877069+cardenluo@users.noreply.github.com>
2026-02-07 19:37:20 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,16 @@
+from .nodes import Unit_PromptWeight
+
+
+
+WEB_DIRECTORY = "./web"
+
+
+
+
+NODE_CLASS_MAPPINGS = {
+    "Unit_PromptWeight": Unit_PromptWeight,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "Unit_PromptWeight": "Unit Prompt Weight",
+}
